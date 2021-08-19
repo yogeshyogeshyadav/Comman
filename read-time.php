74349402a4c8e78ac7e62a4f85b5e5dd08d13f15
Yogesh199520 <75299119+Yogesh199520@users.noreply.github.com>
2021-08-19 18:03:33 +0530

    Update read-time.php

--- read-time.php
+++ read-time.php
@@ -1,14 +1,14 @@
-function reading_time() {
-$content = get_post_field( 'post_content', $post->ID );
-$word_count = str_word_count( strip_tags( $content ) );
-$readingtime = ceil($word_count / 200);
-if ($readingtime == 1) {
-$timer = " minute";
-} else {
-$timer = " minutes";
-}
-$totalreadingtime = $readingtime . $timer;
-return $totalreadingtime;
+function yy_content_read_time() {
+    $content = get_post_field( 'post_content', $post->ID );
+    $word_count = str_word_count( strip_tags( $content ) );
+    $readTime = ceil($word_count / 200);
+    if($readTime == 1) {
+        $timer = " minute";
+    }else{
+        $timer = " minutes";
+    }
+    $totalreadTime = $readTime . $timer;
+    return $totalreadTime;
 }
 
-<?php echo reading_time(); ?>
+<?php echo yy_content_read_time(); ?>
